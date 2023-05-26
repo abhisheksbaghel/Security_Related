5d02dc2f753cd7c6dbaa870ed1f6808e37adb7a2
Abhishek Singh Baghel <92367280+abhisheksbaghel@users.noreply.github.com>
2023-05-26 15:42:53 +0530

    Secured api gateway

---
+++ SecuredGateway/SecuredGateway/src/main/java/com/Gateway/SecuredGateway/SecuredGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.Gateway.SecuredGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredGatewayApplication.class, args);
+	}
+
+}
---
+++ SecuredGateway/SecuredGateway/src/test/java/com/Gateway/SecuredGateway/SecuredGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gateway.SecuredGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
